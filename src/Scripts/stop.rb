0775b1415d1ead67e2623a3e1ec9e71664bfe6e8
betrakiss <angelin.nedelchev@outlook.com>
2016-07-27 22:35:44 +0300

    Ugliest think I've ever written. Does the perfect job...

--- src/Scripts/stop.rb
+++ src/Scripts/stop.rb
@@ -9,3 +9,23 @@ class Stop
     @line = line
   end
 end
+
+class Line
+  attr_accessor :name
+  attr_accessor :directions
+end
+
+class Direction
+  attr_accessor :name
+  attr_accessor :days
+end
+
+class Day
+  attr_accessor :type
+  attr_accessor :stops
+end
+
+class Arrival
+  attr_accessor :name
+  attr_accessor :timings
+end
