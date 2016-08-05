a91499df5c68663ec7584524d3caa2a416b9d462
betrakiss <angelin.nedelchev@outlook.com>
2016-08-06 00:31:02 +0300

    Adds SettingsEntity.

---
+++ src/TramlineFive/TramlineFive/DataAccess/Entities/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyBus.DataAccess.Entities
+{
+    public class Settings : BaseEntity
+    {
+        public int GetId()
+        {
+            return ID;
+        }
+
+        public int ID { get; set; }
+        public bool PushNotifications { get; set; }
+        public bool LiveTile { get; set; }
+    }
+}
--- src/TramlineFive/TramlineFive/DataAccess/TramlineFiveContext.cs
+++ src/TramlineFive/TramlineFive/DataAccess/TramlineFiveContext.cs
@@ -5,10 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SettingsEntity = EasyBus.DataAccess.Entities.Settings;
+
 namespace TramlineFive.DataAccess
 {
     public class TramlineFiveContext : DbContext
     {
+        public DbSet<SettingsEntity> Settings { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Filename=TramlineFive.db");
