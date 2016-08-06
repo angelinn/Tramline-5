286a6e0331de63bb28f1a29286b190d4df88f267
betrakiss <angelin.nedelchev@outlook.com>
2016-08-06 22:32:36 +0300

    Fixes not fetching all day types and non unique stops.

--- src/Scripts/transformer.rb
+++ src/Scripts/transformer.rb
@@ -3,28 +3,30 @@ require_relative 'models'
 class Transformer
   def self.to_logical_hash(data)
     lines = data.flatten.group_by { |s| s.line }
-    lines.each do |key, value|
-      lines[key] = value.group_by { |s| s.direction }
-      lines[key].each { |k, v| lines[key][k] = v.group_by { |g| g.day } }
+    lines.each do |line, line_data|
+      lines[line] = line_data.group_by { |s| s.direction }
+      lines[line].each do |direction, direction_data|
+        lines[line][direction] = direction_data.group_by { |g| g.day }
+      end
     end
   end
 
   def self.to_csharp_ready_hash(hash)
-    hash.map do |key, value|
+    hash.map do |line_key, directions|
       line = Line.new
-      line.name = key
-      line.directions = value.map do |k, v|
+      line.name = line_key
+      line.directions = directions.map do |dir_key, days|
         dir = Direction.new
-        dir.name = k
-        dir.days = v.map do |lelk, lelv|
+        dir.name = dir_key
+        dir.days = days.map do |day_key, stops|
           day = Day.new
-          day.type = lelk
-          day.stops = lelv.map do |arrival|
+          day.type = day_key
+          day.stops = stops.map do |arrival|
             stop = Stop.new
             stop.name = arrival.name
             stop.timings = arrival.timings
             stop
-          end
+          end.uniq { |stop| [stop.name, stop.timings] }
           day
         end
         dir
