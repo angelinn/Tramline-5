b86f0b87aaa9f67ceddd133164d2c1f462b83635
betrakiss <angelin.nedelchev@outlook.com>
2016-08-08 21:44:47 +0300

    Adds IncludeMultiple extension method.

---
+++ src/TramlineFive/TramlineFive.DataAccess/Extensions/IQueryableIncludeMultiple.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TramlineFive.DataAccess.Extensions
+{
+    public static class IQueryableIncludeMultiple
+    {
+        public static IQueryable<T> IncludeMultiple<T>(this IQueryable<T> query, params Expression<Func<T, object>>[] includes) where T : class
+        {
+            if (includes != null)
+            {
+                query = includes.Aggregate(query,
+                          (current, include) => current.Include(include));
+            }
+
+            return query;
+        }
+    }
+}
