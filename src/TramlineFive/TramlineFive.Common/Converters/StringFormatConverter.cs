7f0f42ffebde85992341b72c5a1cc52674086b5a
betrakiss <angelin.nedelchev@outlook.com>
2016-08-16 15:01:35 +0300

    Add StringFormatConverter.

---
+++ src/TramlineFive/TramlineFive.Common/Converters/StringFormatConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace TramlineFive.Common.Converters
+{
+    public class StringFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string formatString = parameter as string;
+            if (!String.IsNullOrEmpty(formatString))
+                return String.Format(formatString, value);
+
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return String.Empty;
+        }
+    }
+}
