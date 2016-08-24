9692d608158d2542c02951b5dda24a1c54a167f4
betrakiss <angelin.nedelchev@outlook.com>
2016-08-24 17:12:01 +0300

    Add fetch time for virtual table.

--- src/TramlineFive/TramlineFive/ViewModels/ArrivalViewModel.cs
+++ src/TramlineFive/TramlineFive/ViewModels/ArrivalViewModel.cs
@@ -14,19 +14,22 @@ namespace TramlineFive.ViewModels
     {
         public ObservableCollection<Arrival> Arrivals { get; set; }
         public StringViewModel StopTitle { get; set; }
+        public StringViewModel AsOfTime { get; set; }
 
         public ArrivalViewModel()
         {
             Arrivals = new ObservableCollection<Arrival>();
             StopTitle = new StringViewModel();
+            AsOfTime = new StringViewModel();
         }
 
         public async Task<bool> GetByStopCode(string stopCode)
         {
             Arrivals.Clear();
             StopTitle.Source = String.Empty;
+            AsOfTime.Source = String.Empty;
 
-            IEnumerable<Arrival> arrivals = await SumcManager.GetByStopAsync(stopCode, new CaptchaDialog());
+            IEnumerable<Arrival> arrivals = await SumcManager.GetByStopAsync(stopCode, typeof(CaptchaDialog));
 
             if (arrivals?.Count() == 0)
                 return false;
@@ -35,6 +38,7 @@ namespace TramlineFive.ViewModels
                 Arrivals.Add(arrival);
 
             StopTitle.Source = SumcParser.ParseStopTitle(Arrivals.FirstOrDefault()?.StopTitle);
+            AsOfTime.Source = "Данни от " + DateTime.Now.ToString("HH:mm");
 
             return true;
         } 
